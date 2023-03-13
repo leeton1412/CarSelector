1800ff2b7d8c948810d85ad00b70b0c1bc24b335
Leeton14 <leeton1412@googlemail.com>
2023-03-13 21:39:24 +0000

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarSelector
+{
+    public class Car
+    {
+        public void TestMessage()
+        {
+            Console.WriteLine("Test");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CarSelector
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Car car = new Car();
+            car.TestMessage();
+        }
+    }
+}
